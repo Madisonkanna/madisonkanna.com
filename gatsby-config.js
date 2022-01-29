32b3173eccc57d0c00c2c7227d066f237e5c1745
Madison Kanna <madisonkanna@gmail.com>
2022-01-29 10:35:59 -0800

    fix: update site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-casper.netlify.com',
+        siteUrl: 'https://madisonkanna.com',
       },
     },
     'gatsby-plugin-emotion',
